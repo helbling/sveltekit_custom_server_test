4fe3367a4da4479830a1006320a9f2840b35280d
Christian Helbling <helbling@search.ch>
2022-05-05 21:27:10 +0200

    add custom server seving static files with custom headers

---
+++ my-server.js
@@ -0,0 +1,22 @@
+import { handler } from './build/handler.js';
+import express from 'express';
+
+const app = express();
+
+// handle static folder ourselves which allows us to set custom headers
+app.use(express.static(
+	'static',
+	{
+		setHeaders: (res, pathname, stats) => {
+            if (pathname.match(/\.png$/))
+                res.setHeader('Access-Control-Allow-Origin', '*');
+        }
+	}
+));
+
+// let SvelteKit handle everything else, including serving prerendered pages and static assets
+app.use(handler);
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
